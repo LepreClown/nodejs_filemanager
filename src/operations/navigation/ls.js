9356b903b9c2a0261e25e4f635ee6e19567d32c1
Ivan Serpuhov <i.serpukhov@atomgroup.io>
2024-10-15 01:09:09 +0300

    fix: Fixed sort ls.js file

--- src/operations/navigation/ls.js
+++ src/operations/navigation/ls.js
@@ -1,21 +1,34 @@
-import fs from "node:fs";
+import fs from "node:fs/promises";
 
 import {ErrorsEnum} from "../../constants/errors.constant.js";
 
+const DIRECTORY = "directory"
+const FILE = "file"
+
 export const ls = async () => {
-  const readAndWriteHandler = () => fs.promises.readdir( process.cwd(), {withFileTypes: true});
+  const readAndWriteHandler = async () => await fs.readdir(process.cwd(), {withFileTypes: true});
   const files = await readAndWriteHandler()
 
   const tableData = files.map((file) => {
-    const type = file.isDirectory() ? "directory" : "file";
+    const type = file.isDirectory() ? DIRECTORY : FILE;
     return {
       name: file.name,
       type,
     }
   })
 
+  const sortedTableData = tableData.sort((a, b) => {
+    if (a.type === DIRECTORY && b.type === FILE) {
+      return -1;
+    } else if (a.type === FILE && b.type ===DIRECTORY) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+
   try {
-    console.table(tableData)
+    console.table(sortedTableData)
   } catch (error) {
     console.error(ErrorsEnum.OPERATION, error);
   }
